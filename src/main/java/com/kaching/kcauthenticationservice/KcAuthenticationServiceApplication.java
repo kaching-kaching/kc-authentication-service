47f66946c24e1eb36546c976a75644ed777f6aba
DuongVu98 <duongvutung1998@gmail.com>
2021-08-17 02:31:09 +0700

    first init

---
+++ src/main/java/com/kaching/kcauthenticationservice/KcAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kaching.kcauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcAuthenticationServiceApplication.class, args);
+    }
+
+}
