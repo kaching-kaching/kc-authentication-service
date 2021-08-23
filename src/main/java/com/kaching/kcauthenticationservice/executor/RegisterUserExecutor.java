29b28dfdd0ff05d76e7ab849b893a80d6d7275b1
DuongVu98 <duongvutung1998@gmail.com>
2021-08-23 22:50:23 +0700

    separate models

--- src/main/java/com/kaching/kcauthenticationservice/executor/RegisterUserExecutor.java
+++ src/main/java/com/kaching/kcauthenticationservice/executor/RegisterUserExecutor.java
@@ -2,35 +2,30 @@ package com.kaching.kcauthenticationservice.executor;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.UserRecord;
 import com.google.firebase.auth.UserRecord.CreateRequest;
 import com.kaching.kcauthenticationservice.command.RegisterUserCommand;
-import com.kaching.kcauthenticationservice.entity.User;
-import com.kaching.kcauthenticationservice.exception.EmailAlreadyExistException;
 import com.kaching.kcauthenticationservice.exception.UnhandledException;
-import com.kaching.kcauthenticationservice.repository.UserRepository;
 import com.kaching.libcommand.core.BaseCommand;
 import com.kaching.libcommand.core.CommandExecutor;
 import com.kaching.libcommand.feature.CommandConvertor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.experimental.FieldDefaults;
 
 @Builder
 @AllArgsConstructor
+@FieldDefaults(makeFinal = true)
 public class RegisterUserExecutor extends CommandExecutor implements CommandConvertor<RegisterUserCommand> {
 
     private FirebaseAuth auth;
-    private UserRepository userRepository;
 
     @Override
     public void execute() {
         var command = convert(getCommand());
-
-        checkEmailExists(command.getEmail());
-        saveUser(saveFirebaseUser(command));
+        saveFirebaseUser(command);
     }
 
-    private UserRecord saveFirebaseUser(RegisterUserCommand command) {
+    private void saveFirebaseUser(RegisterUserCommand command) {
         var request = new CreateRequest()
            .setEmail(command.getEmail())
            .setEmailVerified(false)
@@ -38,24 +33,9 @@ public class RegisterUserExecutor extends CommandExecutor implements CommandConv
            .setDisplayName(command.getUsername())
            .setDisabled(false);
         try {
-            return auth.createUser(request);
+            auth.createUser(request);
         } catch (FirebaseAuthException exception) {
             throw new UnhandledException(exception.getMessage(), exception);
-        }
-    }
-
-    private void saveUser(UserRecord firebaseUser) {
-        var newUser = User.builder()
-           .email(firebaseUser.getEmail())
-           .uid(firebaseUser.getUid())
-           .username(firebaseUser.getDisplayName())
-           .build();
-        userRepository.save(newUser);
-    }
-
-    private void checkEmailExists(String email) {
-        if (userRepository.isEmailExist(email)) {
-            throw new EmailAlreadyExistException(String.format("Email %s already exist", email));
         }
     }
 
