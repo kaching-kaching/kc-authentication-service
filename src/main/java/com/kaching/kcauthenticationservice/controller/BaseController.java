fa442a01ebb71950be6b3618d429afca018d4c54
Phan Minh <38271639+hophanminh@users.noreply.github.com>
2021-09-01 00:25:21 +0700

    Github Webhoook

--- src/main/java/com/kaching/kcauthenticationservice/controller/BaseController.java
+++ src/main/java/com/kaching/kcauthenticationservice/controller/BaseController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class BaseController {
+
     public ResponseEntity<Object> responseOk() {
         return ResponseEntity.ok().build();
     }
